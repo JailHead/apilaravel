7b87ae9775a1314509606d7a14b024ae9e2c9775
JailHead <2122300432@soy.utj.edu.mx>
2024-02-08 19:16:36 -0600

    Finish other postaman test

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Helpers\JwtAuth;
+use App\Models\Car;
 
 class CarController extends Controller
 {
@@ -16,7 +17,11 @@ class CarController extends Controller
         $jwtAuth = new JwtAuth();
         $checkToken = $jwtAuth->checkToken($hash);
         if($checkToken){
-            echo "Index de CarController Autenticado"; die();
+            $cars = Car::all();
+            return response()->json(array(
+                'cars'=>$cars,
+                'status'=>'success'
+            ), 200);
         }else{
             echo "Index de CarController No Autenticado"; die();
         }
@@ -36,7 +41,58 @@ class CarController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $hash =  $request->header('Authorization', null);
+        $jwtAuth = new JwtAuth();
+        $checkToken = $jwtAuth->checkToken($hash);
+
+        if($checkToken){
+            //Recoger los datos por post
+            $json = $request->input('json', null);
+            $params = json_decode($json);
+            $params_array = json_decode($json, true);
+
+            //Conseguir el usuario identificado
+            $user = $jwtAuth->checkToken($hash, true);
+
+            //Validación de laravel
+            $request->merge($params_array);
+            try{
+                $validate = $this->validate($request,[
+                    'title' => 'required',
+                    'description' => 'required',
+                    'price' => 'required',
+                    'status' => 'required'
+                ]);
+
+            }catch (\Illuminate\Validation\ValidationException $e){
+                return $e->getResponse();
+            }
+
+
+            //Guardar el coche
+                $car = new Car();
+                $car->user_id = $user->sub;
+                $car->title = $params->title;
+                $car->description = $params->description;
+                $car->price = $params->price;
+                $car->status = $params->status;
+                $car->save();
+
+                $data = array(
+                    'car'=>$car,
+                    'status' => 'success',
+                    'code' => 200
+                );
+
+        }else{
+            //Devolver un error
+            $data = array(
+                'message' => 'Login incorrecto',
+                'status' => 'success',
+                'code' => 200
+            );
+        }
+        return response()->json($data, 300);
     }
 
     /**
